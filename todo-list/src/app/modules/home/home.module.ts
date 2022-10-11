3eac2a6bd1a284a1ff04255e6a2b40aedb1692d9
elioenay <elioenay99@gmail.com>
2022-10-11 10:46:48 -0400

    Terminando 1° Css

---
+++ todo-list/src/app/modules/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    TodoListComponent,
+    TodoInputAddItensComponent,
+    TodoButtonDeleteAllComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
